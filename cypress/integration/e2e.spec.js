f6c406a1b41a41fc1fe3012c423169171f69a130
André Fernandes <105398833+afernan1000@users.noreply.github.com>
2023-03-27 00:56:35 -0300

    Testes Avançados Mod.12

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+// FAKER PARA NOME, SOBRENOME E EMAIL
+const { faker } = require('@faker-js/faker');
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
         Quero acessar a Loja EBAC 
@@ -9,18 +12,23 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
-    beforeEach(() => {
+    before(() => {
         cy.visit('minha conta')
-        // APONTANDO PARA O PERFIL EM FIXTURE E CRIANDO VARIAVEL DADOS
-        cy.fixture('perfil').then(dados => {
-            // FAZENDO LOGIN COM COMANDOS CUSTOMIZADOS E COM A VARIAVEL DADOS
-            cy.login(dados.usuario, dados.senha)
-        })
     });
+
+    it('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, { log: false })
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.page-title').should('contain', 'Minha conta')
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá,')
+        })
+    })
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
 
-    });
+    })
 
 
 })
